d76437310b974de26e0ff819525fbba9d4348ca0
Vitória Beatriz Batista Brandão <132864073+VitoriaBrandao23@users.noreply.github.com>
2024-10-20 23:15:23 -0300

    Hello World

---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def initial():
+    return 'Olá, mundo!'
+
+@app.route('/<idioma>')
+def frances(idioma):
+    if idioma == 'portugues':
+        mensagem = 'Olá, mundo!'
+    elif idioma == 'ingles':
+        mensagem = 'Hello, world!'
+    elif idioma == 'frances':
+        mensagem = 'Salut, monde!'
+    else:
+        mensagem = 'Não tenho conhecimento!'
+    
+    return f'<h1>{mensagem}</h1>'
